2632cd3843a8240f4367567d112cc354face9e6c
HongyeDeng <139095487+HongyeDeng@users.noreply.github.com>
2025-01-07 22:00:09 +0800

    Delete src directory

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Antd from "ant-design-vue";
-import { createPinia } from "pinia";
-import "ant-design-vue/dist/reset.css";
-
-const piana = createPinia();
-createApp(App).use(piana).use(Antd).use(router).mount("#app");
